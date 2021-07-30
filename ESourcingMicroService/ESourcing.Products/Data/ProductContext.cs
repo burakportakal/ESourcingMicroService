fc0ae8462e477888db82b1aefd001fcacc19ed50
Burak Portakal <buraqportakal@hotmail.com>
2021-07-31 00:30:43 +0300

    Add project files.

---
+++ ESourcingMicroService/ESourcing.Products/Data/ProductContext.cs
@@ -0,0 +1,20 @@
+﻿using ESourcing.Products.Data.Interfaces;
+using ESourcing.Products.Entities;
+using ESourcing.Products.Settings;
+using MongoDB.Driver;
+
+namespace ESourcing.Products.Data
+{
+    public class ProductContext : IProductContext
+    {
+        public ProductContext(IProductDatabaseSettings productDatabaseSettings)
+        {
+            var client = new MongoClient(productDatabaseSettings.ConnectionStrings);
+            var database = client.GetDatabase(productDatabaseSettings.DatabaseName);
+            Products = database.GetCollection<Product>(productDatabaseSettings.CollectionName);
+            ProductContextSeed.SeedData(Products);
+        }
+        
+        public IMongoCollection<Product> Products { get; }
+    }
+}
